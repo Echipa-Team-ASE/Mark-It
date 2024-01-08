4ceee8fa5ae82cb2d5323b5cf9e182146658b52e
Vlad Beraru <vlad.beraru@nevexo.ro>
2024-01-08 19:55:26 +0200

    minor admin changes

--- app/(dashboard)/admin/page.tsx
+++ app/(dashboard)/admin/page.tsx
@@ -8,24 +8,24 @@ export default async function AdminDashboard() {
 
   return (
     <div className="flex min-h-screen flex-col max-w-7xl pt-16 mx-auto p-4">
-      <div>
-        <AddUserPopup
-          managers={managers.map((manager) => {
-            return {
-              name: manager.name,
-              id: manager.id,
-            } as ManagerOption;
-          })}
-        />
-        <UserTable users={users.map((user) => {
+      <AddUserPopup
+        managers={managers.map((manager) => {
+          return {
+            name: manager.name,
+            id: manager.id,
+          } as ManagerOption;
+        })}
+      />
+      <UserTable
+        users={users.map((user) => {
           return {
             name: user.name,
             email: user.email,
             manager: managers.find((manager) => manager.id === user.managerId)?.name,
             role: user.role as any,
           };
-        })} />
-      </div>
+        })}
+      />
     </div>
   );
 }
